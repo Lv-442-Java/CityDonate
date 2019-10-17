ecf6023fd7eefcb2642afc6967edd3c3ee9fc51c
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-17 15:23:31 +0300

    initProject

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/CityDonutApplication.java
@@ -0,0 +1,13 @@
+package com.softserve.ita.java442.cityDonut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityDonutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityDonutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/softserve/ita/java442/cityDonut/CityDonutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.softserve.ita.java442.cityDonut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CityDonutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
