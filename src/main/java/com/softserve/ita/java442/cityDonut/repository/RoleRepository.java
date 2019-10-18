fdfcbd20c0dbe7a0ddbb32eb3e48eccd66d3e3ba
nastiamazurak <nastia.mazurak@gmail.com>
2019-10-18 22:56:53 +0300

    added anotation

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/RoleRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/RoleRepository.java
@@ -2,7 +2,9 @@ package com.softserve.ita.java442.cityDonut.repository;
 
 import com.softserve.ita.java442.cityDonut.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
