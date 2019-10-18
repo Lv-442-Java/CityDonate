313853097ad4e6bb8b9dfd35cfb5039a1f3804a1
NazarRyvak <ryvaknt96@gmail.com>
2019-10-18 14:56:09 +0300

    add lombok to project entity

--- src/main/java/com/softserve/ita/java442/cityDonut/model/Project.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/model/Project.java
@@ -1,14 +1,16 @@
 package com.softserve.ita.java442.cityDonut.model;
 
 import lombok.Builder;
+import lombok.Data;
+import lombok.ToString;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Objects;
 
 @Entity
 @Builder
+@Data
+@ToString(callSuper = true, exclude = {"comments", "donates", "storyBoards", "media", "documents", "users", "categories"})
 @Table(indexes = @Index(columnList = "name, realizationEndDate"))
 public class Project {
 
@@ -16,6 +18,7 @@ public class Project {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(nullable = false)
     private String name;
 
     @Column(length = 1000)
@@ -43,219 +46,45 @@ public class Project {
 
     @ManyToOne(cascade = {
             CascadeType.DETACH, CascadeType.MERGE,
-            CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.LAZY)
+            CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
     @JoinColumn(name = "project_status_id")
     private ProjectStatus projectStatus;
 
-    @OneToMany(mappedBy = "project", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH })
-    private List<Comment> comments = new ArrayList<Comment>();
+    @ManyToOne(cascade = {
+            CascadeType.DETACH, CascadeType.MERGE,
+            CascadeType.PERSIST, CascadeType.REFRESH}, fetch = FetchType.EAGER)
+    @JoinColumn(name = "owner_id")
+    @Column(nullable = false)
+    private User owner;
 
-    @OneToMany(mappedBy = "project", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH })
-    private List<Donate> donates = new ArrayList<Donate>();
+    @OneToMany(mappedBy = "project", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<Comment> comments;
 
-    @OneToMany(mappedBy = "project", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH })
-    private List<StoryBoard> storyBoards = new ArrayList<StoryBoard>();
+    @OneToMany(mappedBy = "project", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<Donate> donates;
 
-    @OneToMany(mappedBy = "project", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH })
-    private List<Media> media = new ArrayList<Media>();
+    @OneToMany(mappedBy = "project", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<StoryBoard> storyBoards;
 
-    @OneToMany(mappedBy = "project", cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
-            CascadeType.REFRESH })
-    private List<Document> documents = new ArrayList<Document>();
+    @OneToMany(mappedBy = "project", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<Media> media;
+
+    @OneToMany(mappedBy = "project", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST,
+            CascadeType.REFRESH})
+    private List<Document> documents;
 
     @ManyToMany
     @JoinTable(name = "user_has_project",
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "user_id"))
-    private List<User> users = new ArrayList<User>();
+    private List<User> users;
 
     @ManyToMany
     @JoinTable(name = "project_has_category",
             joinColumns = @JoinColumn(name = "project_id"),
             inverseJoinColumns = @JoinColumn(name = "category_id"))
-    private List<Category> categories = new ArrayList<Category>();
-
-    public Project() {
-
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public void setLocation(String location) {
-        this.location = location;
-    }
-
-    public String getLocationLatitude() {
-        return locationLatitude;
-    }
-
-    public void setLocationLatitude(String locationLatitude) {
-        this.locationLatitude = locationLatitude;
-    }
-
-    public String getLocationLongitude() {
-        return locationLongitude;
-    }
-
-    public void setLocationLongitude(String locationLongitude) {
-        this.locationLongitude = locationLongitude;
-    }
-
-    public LocalDateTime getCreationDate() {
-        return creationDate;
-    }
-
-    public void setCreationDate(LocalDateTime creationDate) {
-        this.creationDate = creationDate;
-    }
-
-    public LocalDateTime getPublicationDate() {
-        return publicationDate;
-    }
-
-    public void setPublicationDate(LocalDateTime publicationDate) {
-        this.publicationDate = publicationDate;
-    }
-
-    public LocalDateTime getDonationEndDate() {
-        return donationEndDate;
-    }
-
-    public void setDonationEndDate(LocalDateTime donationEndDate) {
-        this.donationEndDate = donationEndDate;
-    }
-
-    public LocalDateTime getRealizationEndDate() {
-        return realizationEndDate;
-    }
-
-    public void setRealizationEndDate(LocalDateTime realizationEndDate) {
-        this.realizationEndDate = realizationEndDate;
-    }
-
-    public ProjectStatus getProjectStatus() {
-        return projectStatus;
-    }
-
-    public void setProjectStatus(ProjectStatus projectStatus) {
-        this.projectStatus = projectStatus;
-    }
-
-    public List<Comment> getComments() {
-        return comments;
-    }
-
-    public void setComments(List<Comment> comments) {
-        this.comments = comments;
-    }
-
-    public List<Donate> getDonates() {
-        return donates;
-    }
-
-    public void setDonates(List<Donate> donates) {
-        this.donates = donates;
-    }
-
-    public List<StoryBoard> getStoryBoards() {
-        return storyBoards;
-    }
-
-    public void setStoryBoards(List<StoryBoard> storyBoards) {
-        this.storyBoards = storyBoards;
-    }
-
-    public List<Media> getMedia() {
-        return media;
-    }
-
-    public void setMedia(List<Media> media) {
-        this.media = media;
-    }
-
-    public List<Document> getDocuments() {
-        return documents;
-    }
-
-    public void setDocuments(List<Document> documents) {
-        this.documents = documents;
-    }
-
-    public List<User> getUsers() {
-        return users;
-    }
-
-    public void setUsers(List<User> users) {
-        this.users = users;
-    }
-
-    public List<Category> getCategories() {
-        return categories;
-    }
-
-    public void setCategories(List<Category> categories) {
-        this.categories = categories;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Project project = (Project) o;
-        return Objects.equals(id, project.id) &&
-                Objects.equals(name, project.name) &&
-                Objects.equals(locationLatitude, project.locationLatitude) &&
-                Objects.equals(locationLongitude, project.locationLongitude);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, name, locationLatitude, locationLongitude);
-    }
-
-    @Override
-    public String toString() {
-        return "Project{" +
-                "id=" + id +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", location='" + location + '\'' +
-                ", locationLatitude='" + locationLatitude + '\'' +
-                ", locationLongitude='" + locationLongitude + '\'' +
-                ", creationDate=" + creationDate +
-                ", publicationDate=" + publicationDate +
-                ", donationEndDate=" + donationEndDate +
-                ", realizationEndDate=" + realizationEndDate +
-                '}';
-    }
-}
+    private List<Category> categories;
