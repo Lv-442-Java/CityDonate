42596ee797cc085b05231e03160b6216a50c053e
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-18 11:03:52 +0300

    add ProjectStatus

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/model/ProjectStatus.java
@@ -0,0 +1,59 @@
+package com.softserve.ita.java442.cityDonut.model;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import java.util.Objects;
+
+@Entity
+@Builder
+@Table(name="project_status")
+public class ProjectStatus {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotNull
+    @Column(name = "status")
+    private String status;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        ProjectStatus that = (ProjectStatus) o;
+        return id.equals(that.id) &&
+                status.equals(that.status);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, status);
+    }
+
+    @Override
+    public String toString() {
+        return "ProjectStatus{" +
+                "id=" + id +
+                ", status='" + status + '\'' +
+                '}';
+    }
+}
