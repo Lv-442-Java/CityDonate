24e924629b662e5375d6bd901b3c9f0d1dbb983f
myzonchuk <myzzon93@gmail.com>
2019-10-18 14:48:39 +0300

    add CategoryRepository

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/CategoryRepository.java
@@ -0,0 +1,6 @@
+package com.softserve.ita.java442.cityDonut.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository {
+}
