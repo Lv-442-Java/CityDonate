e17a364069b3e053f927ecc722ad173815439e91
MPro13 <martaprotsak13@gmail.com>
2019-10-18 22:44:01 +0300

    small fix

--- src/main/java/com/softserve/ita/java442/cityDonut/model/MediaType.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/model/MediaType.java
@@ -22,6 +22,6 @@ public class MediaType {
     @Column(name = "media_type", length = 5, nullable = false)
     private String media_type;
 
-    @OneToMany(fetch = FetchType.EAGER, mappedBy = "mediaType")
-    private List<MediaType> mediaTypeList;
+    @OneToMany(fetch = FetchType.LAZY, mappedBy = "mediaType")
+    private List<Media> media;
 }
