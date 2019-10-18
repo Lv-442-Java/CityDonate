5773bc003a29c8eecd9ef794b51e9397d0f34753
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-18 09:29:11 +0300

    add media enity

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/model/Media.java
@@ -0,0 +1,28 @@
+package com.softserve.ita.java442.cityDonut.model;
+
+import lombok.Builder;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "media")
+@Builder
+public class Media {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+
+    @Column(name = "name", length = 30, nullable = false)
+    private String name;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    private MediaType mediaType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private StoryBoard storyBoard;
+
+}
