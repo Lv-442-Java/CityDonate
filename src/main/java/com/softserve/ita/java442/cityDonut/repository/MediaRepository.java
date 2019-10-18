39113c40794aacde7a86e3c1cb5e73152e0541be
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-18 14:57:49 +0300

    add media repo

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/MediaRepository.java
@@ -0,0 +1,7 @@
+package com.softserve.ita.java442.cityDonut.repository;
+
+import com.softserve.ita.java442.cityDonut.model.Media;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaRepository extends JpaRepository<Media, Long> {
+}
