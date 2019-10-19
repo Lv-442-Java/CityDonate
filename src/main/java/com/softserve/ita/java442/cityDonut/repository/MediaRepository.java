e30914a92fe4626adb751ea8e67019e51d5e14cb
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-19 15:43:24 +0300

    add repo annotation

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/MediaRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/MediaRepository.java
@@ -2,6 +2,8 @@ package com.softserve.ita.java442.cityDonut.repository;
 
 import com.softserve.ita.java442.cityDonut.model.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MediaRepository extends JpaRepository<Media, Long> {
 }
