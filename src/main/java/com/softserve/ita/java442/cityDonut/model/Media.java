175a2192ea4c46bdeedae4fd984406dc55a1014f
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-19 16:08:00 +0300

    media entity fix

--- src/main/java/com/softserve/ita/java442/cityDonut/model/Media.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/model/Media.java
@@ -1,6 +1,6 @@
 package com.softserve.ita.java442.cityDonut.model;
 
-import lombok.Builder;
+import lombok.*;
 
 import javax.persistence.*;
 
@@ -8,22 +8,30 @@ import javax.persistence.*;
 @Table(name = "media",
         indexes = {@Index(name = "media_name_index", columnList = "name", unique = true)})
 @Builder
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(exclude = {"name", "mediaType", "project", "storyBoard"})
+@ToString(exclude = {"project", "storyBoard"})
 public class Media {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
     @Column(name = "name", length = 30, nullable = false)
     private String name;
 
     @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "media_type_id")
     private MediaType mediaType;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
     private Project project;
 
     @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "story_board_id")
     private StoryBoard storyBoard;
 
 }
