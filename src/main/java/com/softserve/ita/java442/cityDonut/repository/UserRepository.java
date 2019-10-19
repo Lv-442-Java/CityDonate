e67aa430e00d2eb30d0777d1569a4f4d182e31c2
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-19 15:34:13 +0300

    add User repository

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.softserve.ita.java442.cityDonut.repository;
+
+import com.softserve.ita.java442.cityDonut.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
