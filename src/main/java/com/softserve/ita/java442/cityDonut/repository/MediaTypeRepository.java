cad41bd810536307a00f6f3c21014990082ce8f0
MPro13 <martaprotsak13@gmail.com>
2019-10-19 13:42:54 +0300

    add @Repository

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/MediaTypeRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/MediaTypeRepository.java
@@ -4,5 +4,6 @@ import com.softserve.ita.java442.cityDonut.model.MediaType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+@Repository
 public interface MediaTypeRepository extends JpaRepository<MediaType, Long> {
 }
