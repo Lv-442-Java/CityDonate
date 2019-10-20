cdff2dc47e176f86591a105814bb197d95940844
myzonchuk <myzzon93@gmail.com>
2019-10-20 09:55:23 +0300

    add generics

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/CategoryRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/CategoryRepository.java
@@ -2,5 +2,5 @@ package com.softserve.ita.java442.cityDonut.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CategoryRepository extends JpaRepository {
+public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
