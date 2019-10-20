31ba9705a00ade72328af9fd2b72057f3f2f1af0
myzonchuk <myzzon93@gmail.com>
2019-10-20 09:57:19 +0300

    add @repository to file

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/CategoryRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/CategoryRepository.java
@@ -1,6 +1,8 @@
 package com.softserve.ita.java442.cityDonut.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 }
