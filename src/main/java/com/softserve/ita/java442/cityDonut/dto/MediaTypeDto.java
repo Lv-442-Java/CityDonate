2d35921ab65be8d27e7666b21b292f9529a76888
MPro13 <martaprotsak13@gmail.com>
2019-10-21 22:39:18 +0300

    add @Data

--- src/main/java/com/softserve/ita/java442/cityDonut/dto/MediaTypeDto.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/MediaTypeDto.java
@@ -1,10 +1,10 @@
 package com.softserve.ita.java442.cityDonut.dto;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Builder
+@Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class MediaTypeDto {
