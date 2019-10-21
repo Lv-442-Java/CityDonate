953923bcf48cfed09add54774394330bfa5f3451
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-21 23:36:38 +0300

    add DonateController

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/DonateController.java
@@ -0,0 +1,10 @@
+package com.softserve.ita.java442.cityDonut.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "/api/v1/donate")
+public class DonateController {
+
+}
