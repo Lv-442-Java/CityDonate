731179edf433425d61766403fe3f07f62a67b9e2
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-21 13:20:46 +0300

    appPropsFix

--- src/main/java/com/softserve/ita/java442/cityDonut/CityDonutApplication.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/CityDonutApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CityDonutApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(CityDonutApplication.class, args);
+		SpringApplication.run(CityDonutApplication.class,args);
 	}
 
 }
