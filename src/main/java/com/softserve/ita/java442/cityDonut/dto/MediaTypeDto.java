e29d46cfde51306f430852562d611dff6656f518
MPro13 <martaprotsak13@gmail.com>
2019-10-21 22:15:47 +0300

    add Media_type mapper and Dto

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/MediaTypeDto.java
@@ -0,0 +1,15 @@
+package com.softserve.ita.java442.cityDonut.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class MediaTypeDto {
+
+        private long id;
+
+        private String media_type;
+}
