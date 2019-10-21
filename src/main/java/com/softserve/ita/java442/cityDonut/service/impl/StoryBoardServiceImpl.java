9517cf9d3afcb1298fcb63e6e71dd69525a515ec
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-21 21:49:43 +0300

    create StoryBoardServiceImpl

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/impl/StoryBoardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.softserve.ita.java442.cityDonut.service.impl;
+
+import com.softserve.ita.java442.cityDonut.service.StoryBoardService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StoryBoardServiceImpl implements StoryBoardService {
+}
