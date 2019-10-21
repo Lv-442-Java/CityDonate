07d9ee67309a524e9e6b326c1e6ce3e758d71865
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-21 22:20:46 +0300

    delete StoryBoardService

--- src/main/java/com/softserve/ita/java442/cityDonut/service/StoryBoardService.java
+++
@@ -1,4 +0,0 @@
-package com.softserve.ita.java442.cityDonut.service;
-
-public interface StoryBoardService {
-}
--- src/main/java/com/softserve/ita/java442/cityDonut/service/impl/StoryBoardServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.softserve.ita.java442.cityDonut.service.impl;
-
-import com.softserve.ita.java442.cityDonut.service.StoryBoardService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class StoryBoardServiceImpl implements StoryBoardService {
-}
