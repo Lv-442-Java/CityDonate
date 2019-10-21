2bf65f107d1a92d8a091439e431c805b84695937
myzonchuk <myzzon93@gmail.com>
2019-10-22 01:31:40 +0300

    create CategoryDto and CategoryMapper

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/CategoryDto.java
@@ -0,0 +1,16 @@
+package com.softserve.ita.java442.cityDonut.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class CategoryDto {
+
+    private long id;
+    private String category;
+
+}
