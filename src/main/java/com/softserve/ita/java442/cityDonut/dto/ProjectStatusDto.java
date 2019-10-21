3134c753e44e8b96b4f7480d882a1b03a970d588
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-21 22:28:52 +0300

    create ProjectStatusDto

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/ProjectStatusDto.java
@@ -0,0 +1,15 @@
+package com.softserve.ita.java442.cityDonut.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProjectStatusDto {
+
+    private long id;
+
+    private String status;
+}
