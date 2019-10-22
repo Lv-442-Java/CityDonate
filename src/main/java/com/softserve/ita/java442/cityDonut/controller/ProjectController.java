8239955d516afadf6349ee53018b0bccfc5b71ef
OlegAgrus <34576707+OlegAgrus@users.noreply.github.com>
2019-10-22 14:14:01 +0300

    project controller developing process 1

--- src/main/java/com/softserve/ita/java442/cityDonut/controller/ProjectController.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/ProjectController.java
@@ -1,9 +1,46 @@
 package com.softserve.ita.java442.cityDonut.controller;
 
+import com.softserve.ita.java442.cityDonut.dto.ProjectDto;
+import com.softserve.ita.java442.cityDonut.dto.ProjectStatusDto;
+import com.softserve.ita.java442.cityDonut.service.ProjectService;
+import com.softserve.ita.java442.cityDonut.service.ProjectStatusService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
+
+    @Autowired
+    private ProjectService projectService;
+
+    @Autowired
+    private ProjectStatusService projectStatusService;
+
+    @PostMapping("/")
+    ProjectDto newProject(@RequestBody ProjectDto project) {
+
+        //Do something with fields...
+
+
+        //Set creation date to now
+        project.setCreationDate(LocalDateTime.now());
+
+        //Set project status to 'not started'
+        //Get initial project status from project status service
+        project.setProjectStatusDto(new ProjectStatusDto());
+
+        //Add project to the database using service
+
+        //Set user owner to project
+
+        //return project with new {id}
+        return project;
+    }
+
 }
