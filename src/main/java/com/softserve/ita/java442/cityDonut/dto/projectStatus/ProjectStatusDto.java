d6b5cfbf3676f6ed5729c703584015a7e04c1ed4
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-23 17:45:38 +0300

    add ProjectStatusDto

--- src/main/java/com/softserve/ita/java442/cityDonut/dto/projectStatus/ProjectStatusDto.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/projectStatus/ProjectStatusDto.java
@@ -1,15 +1,16 @@
-package com.softserve.ita.java442.cityDonut.dto;
+package com.softserve.ita.java442.cityDonut.dto.projectStatus;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+@Builder
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ProjectStatusDto {
 
     private long id;
-
     private String status;
 }
