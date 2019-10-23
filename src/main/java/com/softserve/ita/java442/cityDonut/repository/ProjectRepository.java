0cd7112776e25d4f41b1fde3f5e555ccba271505
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-23 17:51:21 +0300

    add getProjectsByProjectStatusAndMoneyNeededBetween

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/ProjectRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/ProjectRepository.java
@@ -1,9 +1,13 @@
 package com.softserve.ita.java442.cityDonut.repository;
 
 import com.softserve.ita.java442.cityDonut.model.Project;
+import com.softserve.ita.java442.cityDonut.model.ProjectStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProjectRepository extends JpaRepository<Project, Long> {
+    List<Project> getProjectsByProjectStatusAndMoneyNeededBetween(ProjectStatus status, long moneyFrom, long moneyTo);
 }
