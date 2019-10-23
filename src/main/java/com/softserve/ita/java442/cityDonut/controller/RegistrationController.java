0e28517a0d5b872823b14ec5dd7240d08e82f67d
myzonchuk <myzzon93@gmail.com>
2019-10-23 13:29:12 +0300

    merge develop

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/RegistrationController.java
@@ -0,0 +1,9 @@
+package com.softserve.ita.java442.cityDonut.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/registration")
+public class RegistrationController {
+}
