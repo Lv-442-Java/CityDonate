6ce6948c60ab631da4f50b8a14234c2256a139e1
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-23 17:51:48 +0300

    add getFilteredProjects

--- src/main/java/com/softserve/ita/java442/cityDonut/service/ProjectService.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/ProjectService.java
@@ -1,4 +1,9 @@
 package com.softserve.ita.java442.cityDonut.service;
 
+import com.softserve.ita.java442.cityDonut.dto.project.PreviewProjectDto;
+
+import java.util.List;
+
 public interface ProjectService {
+    List<PreviewProjectDto> getFilteredProjects(List<String> categories, long moneyFrom, long moneyTo, String status);
 }
--- src/main/java/com/softserve/ita/java442/cityDonut/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/impl/ProjectServiceImpl.java
@@ -1,13 +1,36 @@
 package com.softserve.ita.java442.cityDonut.service.impl;
 
+import com.softserve.ita.java442.cityDonut.dto.category.CategoryDto;
+import com.softserve.ita.java442.cityDonut.dto.project.PreviewProjectDto;
+import com.softserve.ita.java442.cityDonut.mapper.category.CategoryMapper;
+import com.softserve.ita.java442.cityDonut.mapper.project.PreviewProjectMapper;
 import com.softserve.ita.java442.cityDonut.repository.ProjectRepository;
 import com.softserve.ita.java442.cityDonut.service.ProjectService;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@AllArgsConstructor
 @Service
 public class ProjectServiceImpl implements ProjectService {
 
-    @Autowired
     ProjectRepository projectRepository;
+    ProjectStatusServiceImpl projectStatusService;
+    CategoryServiceImpl categoryService;
+
+    @Override
+    public List<PreviewProjectDto> getFilteredProjects
+            (List<String> categories, long moneyFrom, long moneyTo, String status) {
+        List<PreviewProjectDto> filteredProjects = PreviewProjectMapper.convertListToDto(
+                projectRepository.getProjectsByProjectStatusAndMoneyNeededBetween(
+                        projectStatusService.getByStatus(status), moneyFrom, moneyTo));
+        filterByCategories(filteredProjects,
+                CategoryMapper.convertListToDto(categoryService.getCategoriesByCategories(categories)));
+        return filteredProjects;
+    }
+
+    private void filterByCategories(List<PreviewProjectDto> previewProjectDtos, List<CategoryDto> categoryDtos) {
+        previewProjectDtos.removeIf(previewProjectDto -> !previewProjectDto.getCategories().containsAll(categoryDtos));
+    }
 }
