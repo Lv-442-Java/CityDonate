9bf3c3afb0cb8e506b9cac4818b525f1e0244a4d
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-24 13:56:00 +0300

    add getProjectById

--- src/main/java/com/softserve/ita/java442/cityDonut/controller/ProjectController.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/ProjectController.java
@@ -1,9 +1,22 @@
 package com.softserve.ita.java442.cityDonut.controller;
 
+import com.softserve.ita.java442.cityDonut.dto.project.MainProjectInfoDto;
+import com.softserve.ita.java442.cityDonut.service.impl.ProjectServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/project")
 public class ProjectController {
+
+    @Autowired
+    ProjectServiceImpl projectService;
+
+    @GetMapping("/{id}")
+    MainProjectInfoDto getProjectById (@PathVariable long id){
+        return projectService.getProjectById(id);
+    }
 }
