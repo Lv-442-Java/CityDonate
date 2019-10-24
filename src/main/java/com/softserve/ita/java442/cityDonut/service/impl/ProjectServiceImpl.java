5b730a4d4d9f8a823d8de00e6947f4fcd425867c
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-24 13:55:39 +0300

    add getProjectById

--- src/main/java/com/softserve/ita/java442/cityDonut/service/impl/ProjectServiceImpl.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/impl/ProjectServiceImpl.java
@@ -1,5 +1,7 @@
 package com.softserve.ita.java442.cityDonut.service.impl;
 
+import com.softserve.ita.java442.cityDonut.dto.project.MainProjectInfoDto;
+import com.softserve.ita.java442.cityDonut.mapper.project.MainProjectInfoMapper;
 import com.softserve.ita.java442.cityDonut.repository.ProjectRepository;
 import com.softserve.ita.java442.cityDonut.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,4 +12,10 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Autowired
     ProjectRepository projectRepository;
+
+    @Override
+    public MainProjectInfoDto getProjectById(long id) {
+        MainProjectInfoMapper mainProjectInfoMapper = new MainProjectInfoMapper();
+        return mainProjectInfoMapper.convertToDto(projectRepository.getById(id));
+    }
 }
