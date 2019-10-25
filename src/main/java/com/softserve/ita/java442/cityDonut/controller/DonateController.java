5349978e3815ad67abf7c7438fa065706da9cc1a
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-25 17:15:39 +0300

    little fix donate controller and service

--- src/main/java/com/softserve/ita/java442/cityDonut/controller/DonateController.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/DonateController.java
@@ -1,16 +1,14 @@
 package com.softserve.ita.java442.cityDonut.controller;
 
 import com.softserve.ita.java442.cityDonut.dto.donateDto.DonateDto;
-import com.softserve.ita.java442.cityDonut.model.Project;
-import com.softserve.ita.java442.cityDonut.service.DonateService;
 import com.softserve.ita.java442.cityDonut.service.impl.DonateServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.awt.color.ProfileDataException;
 import java.util.List;
 
 @RestController
@@ -18,14 +16,17 @@ import java.util.List;
 
 public class DonateController {
 
-    @Autowired
+    private final
     DonateServiceImpl donateService;
 
+    public DonateController(DonateServiceImpl donateService) {
+        this.donateService = donateService;
+    }
 
 
     @GetMapping("/project/{id}")
-    public List<DonateDto> getProjectDonates(@PathVariable long id) {
-        return donateService.getProjectDonates(id);
+    public ResponseEntity<List<DonateDto>> getProjectDonates(@PathVariable long id) {
+        return new ResponseEntity<>(donateService.getProjectDonates(id), HttpStatus.OK);
     }
 
 }
--- src/main/java/com/softserve/ita/java442/cityDonut/service/impl/DonateServiceImpl.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/impl/DonateServiceImpl.java
@@ -1,19 +1,15 @@
 package com.softserve.ita.java442.cityDonut.service.impl;
 
 import com.softserve.ita.java442.cityDonut.dto.donateDto.DonateDto;
-import com.softserve.ita.java442.cityDonut.mapper.GeneralMapper;
 import com.softserve.ita.java442.cityDonut.mapper.donate.DonateMapper;
 import com.softserve.ita.java442.cityDonut.model.Donate;
-import com.softserve.ita.java442.cityDonut.model.Project;
 import com.softserve.ita.java442.cityDonut.repository.DonateRepository;
-import com.softserve.ita.java442.cityDonut.repository.ProjectRepository;
 import com.softserve.ita.java442.cityDonut.service.DonateService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Optional;
+
 
 @Service
 public class DonateServiceImpl implements DonateService {
@@ -30,7 +26,7 @@ public class DonateServiceImpl implements DonateService {
         List<Donate> donates = donateRepository.getByProjectId(id);
         System.out.println(donates);
         List<DonateDto> donateDtos = new LinkedList<>();
-        for (Donate donate: donates) {
+        for (Donate donate : donates) {
             donateDtos.add(new DonateMapper().convertToDto(donate));
         }
         return donateDtos;
