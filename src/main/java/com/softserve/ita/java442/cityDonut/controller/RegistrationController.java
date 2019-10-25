15bad94ff6e040efd594ead58a2ac092b3f790ea
myzonchuk <myzzon93@gmail.com>
2019-10-25 08:15:53 +0300

    add RegistrationController

--- src/main/java/com/softserve/ita/java442/cityDonut/controller/RegistrationController.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/RegistrationController.java
@@ -1,9 +1,13 @@
 package com.softserve.ita.java442.cityDonut.controller;
 
+import com.softserve.ita.java442.cityDonut.service.impl.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/api/v1/registration")
 public class RegistrationController {
+    @Autowired
+    UserServiceImpl userServiceImpl;
 }
