a4ef90e3d9e28a9134f10638c471de29d500a431
myzonchuk <myzzon93@gmail.com>
2019-10-28 10:12:09 +0200

    add userRegistrationDto

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/user/UserRegistrationDto.java
@@ -0,0 +1,25 @@
+package com.softserve.ita.java442.cityDonut.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistrationDto {
+
+    private long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+
+}
+
