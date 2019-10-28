2ce51f81879a7b89dfe36380fcc975423a7d0026
MPro13 <martaprotsak13@gmail.com>
2019-10-28 10:22:10 +0200

    Modify CityDonutApp

--- src/main/java/com/softserve/ita/java442/cityDonut/CityDonutApplication.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/CityDonutApplication.java
@@ -1,5 +1,6 @@
 package com.softserve.ita.java442.cityDonut;
 
+import com.softserve.ita.java442.cityDonut.dto.media.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
