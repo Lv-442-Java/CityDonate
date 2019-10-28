f39b4e99a39841660d9b330e1e69850f8ae16fcb
myzonchuk <myzzon93@gmail.com>
2019-10-28 19:12:44 +0200

    add new CONST to ErrorMessage

--- src/main/java/com/softserve/ita/java442/cityDonut/constant/ErrorMessage.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/constant/ErrorMessage.java
@@ -5,6 +5,6 @@ public class ErrorMessage {
     public static final String PROJECT_NOT_FOUND_BY_ID = "The project does not exist by this id";
     public static final String INCORRECT_USER_PASSWORD = "The user`s password is incorrect: ";
     public static final String INCORRECT_EMAIL = "Such email exists: ";
-
+    public static final String INVALID_EMAIL = "Email is wrong: ";
 
 }
