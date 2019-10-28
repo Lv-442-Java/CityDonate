d41d3e59fc59fea56fbc2c63e692f0bf97ec2428
MPro13 <martaprotsak13@gmail.com>
2019-10-28 15:37:42 +0200

    delete unhandled exception

--- src/main/java/com/softserve/ita/java442/cityDonut/service/FileStorageService.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/FileStorageService.java
@@ -3,11 +3,9 @@ package com.softserve.ita.java442.cityDonut.service;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.net.MalformedURLException;
-
 public interface FileStorageService {
 
     String storeFile(MultipartFile file);
 
-    Resource loadFileAsResource(String fileName) throws MalformedURLException;
+    Resource loadFileAsResource(String fileName);
 }
