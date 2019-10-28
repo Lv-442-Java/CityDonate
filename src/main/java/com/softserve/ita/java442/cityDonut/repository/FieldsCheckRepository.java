f86afb4b64636016504171e2ab24dceb83a4cfbd
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-28 17:19:56 +0200

    add FieldsCheckRepository

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/FieldsCheckRepository.java
@@ -0,0 +1,12 @@
+package com.softserve.ita.java442.cityDonut.repository;
+
+import com.softserve.ita.java442.cityDonut.model.FieldsCheck;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface FieldsCheckRepository extends JpaRepository<FieldsCheck,Long> {
+    List<FieldsCheck> getAllByProject_Id(long project_id);
+}
