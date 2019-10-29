b2eb9e8359a842be4728c50c734c32e5544db358
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-29 11:26:20 +0200

    add project by user donate dto and mapper

---
+++ src/main/java/com/softserve/ita/java442/cityDonut/mapper/project/ProjectByUserDonateMapper.java
@@ -0,0 +1,33 @@
+package com.softserve.ita.java442.cityDonut.mapper.project;
+
+import com.softserve.ita.java442.cityDonut.dto.project.ProjectByUserDonateDto;
+import com.softserve.ita.java442.cityDonut.mapper.GeneralMapper;
+import com.softserve.ita.java442.cityDonut.model.DonatedUserProject;
+import com.softserve.ita.java442.cityDonut.model.Project;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ProjectByUserDonateMapper implements GeneralMapper<Project, ProjectByUserDonateDto> {
+
+    public ProjectByUserDonateDto convertToDto(Project project, DonatedUserProject donatedUserProject) {
+        return ProjectByUserDonateDto.builder()
+                .id(project.getId())
+                .name(project.getName())
+                .media(project.getMedia())
+                .projectStatus(project.getProjectStatus())
+                .donateCount(donatedUserProject.getDonateCount())
+                .donateSum(donatedUserProject.getSum())
+                .build();
+    }
+
+    @Override
+    public ProjectByUserDonateDto convertToDto(Project model) {
+        return null;
+    }
+
+
+    @Override
+    public Project convertToModel(ProjectByUserDonateDto dto) {
+        return null;
+    }
+}
