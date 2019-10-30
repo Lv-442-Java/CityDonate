d2b9502f491b34157bd6199f0fe4c04f3378a6bf
Nazar Hladysh <nazar.gladish@gmail.com>
2019-10-30 18:27:32 +0200

    add method getByProjectIdAndUserId

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/DonateRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/DonateRepository.java
@@ -1,6 +1,7 @@
 package com.softserve.ita.java442.cityDonut.repository;
 
 import com.softserve.ita.java442.cityDonut.model.Donate;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +9,6 @@ import java.util.List;
 
 @Repository
 public interface DonateRepository extends JpaRepository<Donate, Long> {
-    List<Donate> getByProjectId(long id);
+    List<Donate> getByProjectId(long id, Pageable pageable);
+    List<Donate> getByProjectIdAndUserId(long id, long userId);
 }
