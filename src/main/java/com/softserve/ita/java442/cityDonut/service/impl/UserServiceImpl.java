3cf26bee41336f85de21b83d279f5a6480819bd6
NazarRyvak <ryvaknt96@gmail.com>
2019-10-30 15:41:53 +0200

    add validator and fix something

--- src/main/java/com/softserve/ita/java442/cityDonut/service/impl/UserServiceImpl.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/impl/UserServiceImpl.java
@@ -18,6 +18,7 @@ import com.softserve.ita.java442.cityDonut.validator.EmailValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class UserServiceImpl implements UserService {
@@ -38,6 +39,7 @@ public class UserServiceImpl implements UserService {
     private UserEditMapper userEditMapper;
 
     @Override
+    @Transactional
     public UserEditDto update(UserEditDto userEditDto) {
         User user;
         if (userEditDto != null) {
@@ -46,6 +48,9 @@ public class UserServiceImpl implements UserService {
                     .orElseThrow(() -> new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_ID + userEditDto.getId()));
         } else {
             throw new NotFoundException(ErrorMessage.USER_NOT_FOUND_BY_ID);
+        }
+        if (!emailValidator.validateEmail(userEditDto.getEmail())) {
+            throw new InvalidEmailException(ErrorMessage.INVALID_EMAIL);
         }
         user.setFirstName(userEditDto.getFirstName());
         user.setLastName(userEditDto.getLastName());
@@ -61,6 +66,7 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public void changePassword(UserEditPasswordDto userEditPasswordDto) {
         User user;
         if (userEditPasswordDto != null) {
