759e8d53174b2c96319e5510009cf2f096bbbec1
martavoloshyn <martavoloshyn73@gmail.com>
2019-10-30 23:45:52 +0200

    remove unnecessary methods

--- src/main/java/com/softserve/ita/java442/cityDonut/repository/ProjectStatusRepository.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/repository/ProjectStatusRepository.java
@@ -6,5 +6,4 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ProjectStatusRepository extends JpaRepository<ProjectStatus, Long> {
-    ProjectStatus getProjectStatusByStatus(String status);
 }
