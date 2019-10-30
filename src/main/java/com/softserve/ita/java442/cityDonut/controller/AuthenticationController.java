ed04a2ce27269ae5fef97cc21a3188b7aaa4a897
VOleh <vanivsky.oleh.lv@gmail.com>
2019-10-30 07:59:00 +0200

    Fixed jwt authentification

--- src/main/java/com/softserve/ita/java442/cityDonut/controller/AuthenticationController.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/controller/AuthenticationController.java
@@ -4,41 +4,40 @@ import com.softserve.ita.java442.cityDonut.dto.authentication.AuthenticationRequ
 import com.softserve.ita.java442.cityDonut.model.User;
 import com.softserve.ita.java442.cityDonut.security.JWTTokenProvider;
 import com.softserve.ita.java442.cityDonut.service.impl.UserServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 
 import java.nio.file.attribute.UserPrincipalNotFoundException;
 import java.util.HashMap;
 import java.util.Map;
 
 @RestController
-@RequestMapping(value="/auth",method = RequestMethod.POST)
-
+@RequestMapping("/auth")
 public class AuthenticationController {
 
     private AuthenticationManager authenticationManager;
     private JWTTokenProvider jwtTokenProvider;
     private UserServiceImpl userService;
 
-    public AuthenticationController(AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider, UserServiceImpl userService) {
+    public AuthenticationController(AuthenticationManager authenticationManager, JWTTokenProvider jwtTokenProvider,
+                                    UserServiceImpl userService) {
         this.authenticationManager = authenticationManager;
         this.jwtTokenProvider = jwtTokenProvider;
         this.userService = userService;
     }
-
+    @PostMapping
     public ResponseEntity login(@RequestBody AuthenticationRequestDto requestDto){
         try{
             String userEmail = requestDto.getUserEmail();
-            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userEmail,requestDto.getPassword()));
+            System.out.println(requestDto.getUserEmail());
+            System.out.println(jwtTokenProvider.passwordEncoder().encode(requestDto.getUserEmail()));
+            System.out.println("Chek "+authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(userEmail,
+                    jwtTokenProvider.passwordEncoder().encode(requestDto.getPassword()))).isAuthenticated());
             User user = userService.findUserByEmail(userEmail);
             System.out.println("User controller "+user);
 
--- src/main/java/com/softserve/ita/java442/cityDonut/security/JWTTokenProvider.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/security/JWTTokenProvider.java
@@ -6,9 +6,12 @@ import com.softserve.ita.java442.cityDonut.service.UserService;
 import exeption.JwtAuthenticationExeption;
 import io.jsonwebtoken.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
@@ -89,4 +92,8 @@ public class JWTTokenProvider {
         return claims.getSubject();
     }
 
+    @Bean
+    public BCryptPasswordEncoder passwordEncoder(){
+        return  new BCryptPasswordEncoder();
+    }
 }
