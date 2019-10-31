4c009dee6cdb385b4f1ad4e9e32d7800f53949ff
nastiamazurak <nastia.mazurak@gmail.com>
2019-10-31 09:11:07 +0200

    changed get method in storyBoard controller

--- src/main/java/com/softserve/ita/java442/cityDonut/mapper/storyBoard/StoryBoardMapper.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/mapper/storyBoard/StoryBoardMapper.java
@@ -8,6 +8,9 @@ import com.softserve.ita.java442.cityDonut.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Component
 public class StoryBoardMapper implements GeneralMapper<StoryBoard, StoryBoardDto> {
 
@@ -40,5 +43,13 @@ public class StoryBoardMapper implements GeneralMapper<StoryBoard, StoryBoardDto
                 .moneySpent(dto.getMoneySpent())
                 .isVerified(dto.isVerified())
                 .build();
+    }
+
+    public List<StoryBoardDto> convertListToDto(List<StoryBoard> storyBoards) {
+        List<StoryBoardDto> dtoList = new ArrayList<>();
+        for (StoryBoard storyBoard : storyBoards) {
+            dtoList.add(convertToDto(storyBoard));
+        }
+        return dtoList;
     }
 }
