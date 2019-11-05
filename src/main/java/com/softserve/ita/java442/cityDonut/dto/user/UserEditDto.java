274d0bf53f4ef5ce28403448c607c0060458ca1b
myzonchuk <myzzon93@gmail.com>
2019-11-05 13:01:39 +0200

    delete empty rows

--- src/main/java/com/softserve/ita/java442/cityDonut/dto/user/UserEditDto.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/user/UserEditDto.java
@@ -1,13 +1,12 @@
 package com.softserve.ita.java442.cityDonut.dto.user;
 
-        import com.softserve.ita.java442.cityDonut.constant.ErrorMessage;
-        import lombok.AllArgsConstructor;
-        import lombok.Builder;
-        import lombok.Data;
-        import lombok.NoArgsConstructor;
-
-        import javax.validation.constraints.NotNull;
-        import javax.validation.constraints.Size;
+import com.softserve.ita.java442.cityDonut.constant.ErrorMessage;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Data
 @Builder
