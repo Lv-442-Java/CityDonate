d340049b7e5f273980fe7ef4eae4cae25d0e20b3
myzonchuk <myzzon93@gmail.com>
2019-11-05 12:51:27 +0200

    changed enum ACTIVATE and NOT_ACTIVATE

--- src/main/java/com/softserve/ita/java442/cityDonut/dto/user/UserEditDto.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/dto/user/UserEditDto.java
@@ -1,4 +1,37 @@
 package com.softserve.ita.java442.cityDonut.dto.user;
 
+        import com.softserve.ita.java442.cityDonut.constant.ErrorMessage;
+        import lombok.AllArgsConstructor;
+        import lombok.Builder;
+        import lombok.Data;
+        import lombok.NoArgsConstructor;
+
+        import javax.validation.constraints.NotNull;
+        import javax.validation.constraints.Size;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
 public class UserEditDto {
+
+    private long id;
+
+    private String email;
+
+    @Size(
+            min = 4,
+            max = 50,
+            message = ErrorMessage.INCORRECT_SIZE
+    )
+    @NotNull(message = ErrorMessage.FIELD_CANT_BE_NULL)
+    private String firstName;
+
+    @Size(
+            min = 4,
+            max = 50,
+            message = ErrorMessage.INCORRECT_SIZE
+    )
+    @NotNull(message = ErrorMessage.FIELD_CANT_BE_NULL)
+    private String lastName;
 }
