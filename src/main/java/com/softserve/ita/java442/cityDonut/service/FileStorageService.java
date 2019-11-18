5fb8a86a9110f71b1516639ad15705496d0d99f3
MPro13 <martaprotsak13@gmail.com>
2019-11-18 16:33:24 +0200

    get all files for project

--- src/main/java/com/softserve/ita/java442/cityDonut/service/FileStorageService.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/FileStorageService.java
@@ -1,12 +1,13 @@
 package com.softserve.ita.java442.cityDonut.service;
 
+import com.softserve.ita.java442.cityDonut.dto.media.MediaDto;
 import com.softserve.ita.java442.cityDonut.model.Media;
 import org.springframework.core.io.Resource;
 import org.springframework.web.multipart.MultipartFile;
 
 public interface FileStorageService {
 
-    String storeFile(MultipartFile file, long projectId);
+    MediaDto storeFile(MultipartFile file, long projectId);
 
     Resource loadFileAsResource(String fileName, long fileId);
 }
