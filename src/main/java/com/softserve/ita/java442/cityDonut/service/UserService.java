b66da734c143552cbd2d1f9e111a3fb4d1681179
nastiamazurak <nastia.mazurak@gmail.com>
2019-12-02 13:20:41 +0200

    temo commit

--- src/main/java/com/softserve/ita/java442/cityDonut/service/UserService.java
+++ src/main/java/com/softserve/ita/java442/cityDonut/service/UserService.java
@@ -1,11 +1,13 @@
 package com.softserve.ita.java442.cityDonut.service;
 
+import com.softserve.ita.java442.cityDonut.dto.project.PreviewProjectDto;
 import com.softserve.ita.java442.cityDonut.dto.project.ProjectInfoDto;
 import com.softserve.ita.java442.cityDonut.dto.user.UserEditDto;
 import com.softserve.ita.java442.cityDonut.dto.user.UserRoleDto;
 import com.softserve.ita.java442.cityDonut.model.User;
 import com.softserve.ita.java442.cityDonut.dto.user.UserEditPasswordDto;
 import com.softserve.ita.java442.cityDonut.dto.user.UserRegistrationDto;
+import org.springframework.data.domain.Pageable;
 
 import java.util.List;
 
